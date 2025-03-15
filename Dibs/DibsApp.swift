8c1c38b45f0d5acf5b92c8778c06ee463942d300
Minesh Sumair <mineshsumair@gmail.com>
2025-03-15 13:24:13 -0700

    Add project files

---
+++ Dibs/DibsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DibsApp.swift
+//  Dibs
+//
+//  Created by minesh sumair on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DibsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DibsTests/DibsTests.swift
@@ -0,0 +1,17 @@
+//
+//  DibsTests.swift
+//  DibsTests
+//
+//  Created by minesh sumair on 3/15/25.
+//
+
+import Testing
+@testable import Dibs
+
+struct DibsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
