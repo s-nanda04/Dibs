9d58ef9bf1f6d5ed8e411765b49064662c406e8b
Jia Amarnani <jiaamarnani@gmail.com>
2025-03-15 22:14:31 -0700

    Add ListItemView page

---
+++ Dibs/ListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListItemView.swift
+//  Dibs
+//
+//  Created by Jia Amarnani on 3/15/25.
+//
+
+import SwiftUI
+
+struct ListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListItemView()
+}
